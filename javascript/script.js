dc534fe0508cb8ff4f7fdf31f50a7a296d392c77
Ale <ale@aleLinux>
2017-05-22 21:52:48 +0200

    Completata e testata chiamata ajax ed estrazione  dati

--- javascript/script.js
+++ javascript/script.js
@@ -7,11 +7,17 @@ $(document).ready(function() {
 
     $('button').on('click', function() {
         $.ajax({
-                url: 'https://wind-bow.gomix.me/twitch-api/games/top?callback=?',
+                url: 'https://wind-bow.gomix.me/twitch-api/streams/OgamingSC2?callback=?',
                 type: 'GET',
+                dataType: 'jsonp'
             })
-            .done(function() {
+            .done(function(data) {
                 console.log("success");
+                $('body').append('<p>Id: '+data.stream._id+'</p>');
+                $('body').append('<p>Game: '+data.stream.game+'</p>');
+                $('body').append('<img src="'+data.stream.preview.medium+'" alt="miniatura"></img>');
+                $('body').append('<p>Nome: '+data.stream.channel.name+'</p>');
+                $('body').append('<img src="'+data.stream.channel.logo+'" alt="logo"></p>');
             })
             .fail(function() {
                 console.log("error");
